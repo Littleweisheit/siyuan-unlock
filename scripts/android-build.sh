a94eefdea11909e7a2c01c06e1fe3f546c8f7f3a
ReMinds <11383988+Littleweisheit@users.noreply.github.com>
2024-11-30 16:40:02 +0800

    Update android-build.sh

--- scripts/android-build.sh
+++ scripts/android-build.sh
@@ -9,7 +9,7 @@ rm -rf android/app/libs
 rm -rf android/app/src/main/assets
 echo 'Building UI'
 cd app
-npm install && npm run build
+pnpm install && pnpm run build
 cd ..
 
 echo 'Building Kernel'
@@ -19,6 +19,11 @@ mkdir -p android/app/src/main/assets
 
 cd kernel
 export GOPROXY=https://goproxy.io
+export ANDROID_NDK_HOME=/opt/android-sdk/ndk/27.2.12479018/
+export PATH=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
+export CC=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android24-clang
+export CXX=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android24-clang++
+export PATH=$PATH:~/go/bin
 gomobile bind --tags fts5 -ldflags '-s -w' -v -o ../android/app/libs/kernel.aar -target='android/arm64' -androidapi 24 ./mobile/
 cd ..
 rm android/app/libs/kernel-sources.jar
